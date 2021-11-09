f185a661131d4ae460607013d55006d8ab9727e1
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-09 16:45:49 +0530

    Add files via upload

---
+++ Enemy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1 @click="changeEmoji">{{emoji}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorl',
+  props:{
+    emoji:String,
+  },
+  methods:{
+      changeEmoji(){
+          this.$emit("emojiChange","")
+      }
+
+  }
+}
+</script>
+
+
---
+++ HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <progress  :value="value" max="100"> 32% </progress>
+    
+    
+    <Enemy v-if="value>70 && value<=100" @emojiChange="valueChange" emoji="👿"/>
+    <Enemy v-else-if="value>40 && value<=70" @emojiChange="valueChange" emoji="😭"/>
+    <Enemy v-else-if="value>10 && value<=40" @emojiChange="valueChange" emoji="🤕"/>
+    <Enemy v-else @emojiChange="valueChange" emoji="😵"/>
+
+  </div>
+</template>
+
+<script>
+import Enemy from "./Enemy"
+export default {
+  name: 'HelloWorl',
+  components: {
+    Enemy
+  },
+  data(){
+    return {
+      value:100
+
+    }
+  },
+  methods:{
+    valueChange(){
+      this.value-=10
+    },
+
+
+  }
+}
+</script>
+
+
